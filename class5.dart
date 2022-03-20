19388fdba3fb295aff4bf12da8fcec41bf622573
MuhammadMaaz9 <100517667+MuhammadMaaz9@users.noreply.github.com>
2022-03-20 17:28:20 +0500

    Add files via upload

---
+++ class5.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+void main() {
+  // for (var i = 1; i <= 10; i++) {
+  //   var no = i * 2;
+  //   print('2 x $i = $no');
+  // }
+  /*print('===============FOR IN LOOP====================');
+  var list_1 = [100, 200, 300, 400, 500];
+  for (var n in list_1) {
+    print(n);
+  }*/
+  /*print('=============EVEN NUMBER CLASS TASK===================');
+  List<int> list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (var i in list_1) {
+    if (i % 2 == 0) print(i);
+  }*/
+  /*print('===================CLASS TASK=================');
+  List<dynamic> my_arr = [1, 56, 77, 56, 2, 11, 99, 45, 10];
+  var max_value = my_arr[0];
+  var min_value = my_arr[0];
+  for (var i = 0; i < my_arr.length; i++) {
+    if (max_value < my_arr[i]) max_value = my_arr[i];
+  }
+  for (var i = 0; i < my_arr.length; i++) {
+    if (min_value > my_arr[i]) min_value = my_arr[i];
+  }
+  print(max_value);
+  print(min_value);*/
+   int i = 0;
+   while (i <= 5) {
+    print(i);
+    i++;
+   }
+
+  //   } while ();
+  }
+}
